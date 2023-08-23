9f2a87c116412a9b2e006e93ea6a9b60c6409446
Her0Class08 <baswans811@proton.me>
2023-08-23 18:32:11 -0500

    Added student counting and naming in description

--- venv/Home.py
+++ venv/Home.py
@@ -1,3 +1,35 @@
 import streamlit as st
+import pandas as pd
 
-st.header('sup')
+
+# opens, reads and counts all-students
+def student_number():
+    file = open("info/all-students.txt")
+    list = file.read()
+    list.strip()
+    number = len(list.split())
+    return number
+
+
+def allstudents():
+    file = open("info/all-students.txt")
+    list = file.read()
+    list.strip()
+    return list
+
+
+def allcoaches():
+    file = open("info/all-coaches.txt")
+
+
+
+# page configuration
+st.set_page_config(layout='wide')
+c1 = st.columns(3)
+
+# main description
+main_des = f"Our program has {student_number()} students: {allstudents()}"
+
+# top of site
+st.header('PACT robotics program')
+st.write(main_des)
