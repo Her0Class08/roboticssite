8f587e9dd0f4e8bbffe8591c99c5ac7ae8c054e0
Her0Class08 <baswans811@proton.me>
2023-08-23 18:46:52 -0500

    Added coach counting and naming in description also added 'and' to .txt files

--- venv/Home.py
+++ venv/Home.py
@@ -8,18 +8,27 @@ def student_number():
     list = file.read()
     list.strip()
     number = len(list.split())
-    return number
+    return number - 1
 
 
 def allstudents():
     file = open("info/all-students.txt")
     list = file.read()
-    list.strip()
     return list
+
+
+def coach_number():
+    file = open("info/all-coaches.txt")
+    list = file.read()
+    number = len(list.split())
+    return number - 1
 
 
 def allcoaches():
     file = open("info/all-coaches.txt")
+    list = file.read()
+    list.strip()
+    return list
 
 
 
@@ -28,7 +37,7 @@ st.set_page_config(layout='wide')
 c1 = st.columns(3)
 
 # main description
-main_des = f"Our program has {student_number()} students: {allstudents()}"
+main_des = f"Our program has {student_number()} students: {allstudents()} and {coach_number()} coaches: {allcoaches()}"
 
 # top of site
 st.header('PACT robotics program')
