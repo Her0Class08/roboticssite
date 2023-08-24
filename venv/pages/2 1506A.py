f43d74dd55d82b20b0480f0d4657981598b7bdb2
Her0Class08 <baswans811@proton.me>
2023-08-24 11:27:38 -0500

    added new files to git

---
+++ venv/pages/2 1506A.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+content_d = """
+We are the best. We are the most elite group of young men and women to ever walk this earth. We also have the three of the most supportive 
+parents in the world(Mr. Swanson, Jessica, and noahs mom)
+"""
+
+st.header('Our team')
+st.write(content_d)
